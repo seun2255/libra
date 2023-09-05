f1148eea4766775d9d1180465f43e9f600fd6d69
Primal <81525899+Seun2255@users.noreply.github.com>
2023-09-05 08:55:40 +0100

    feat: imported and created modules for assets

---
+++ client/app/_assets/icons/icons.js
@@ -0,0 +1,63 @@
+import cancel from "./cancel.svg";
+import check from "./check.svg";
+import logo from "./logo.svg";
+import settings from "./settings.svg";
+import search from "./search.svg";
+import upload from "./upload.svg";
+import share from "./share.svg";
+import trash from "./trash.svg";
+import expand from "./expand.svg";
+import drag from "./drag.svg";
+import menu from "./menu.svg";
+import profile from "./profile.svg";
+import schedule from "./schedule.svg";
+import back from "./back.svg";
+import thumbnail from "./thumbnail.svg";
+import tick from "./tick.svg";
+import lock from "./lock.svg";
+import scrollArrow from "./scrollArrow.svg";
+import succesTick from "./succesTick.svg";
+import file from "./file.svg";
+import plus from "./plus.svg";
+import logoText from "./logoText.svg";
+import create from "./create.svg";
+import finance from "./finance.svg";
+import home from "./home.svg";
+import xdcLogo from "./xdcLogo.svg";
+import change from "./switch.svg";
+import arrowDown from "./arrowDown.svg";
+import requests from "./requests.svg";
+
+const icons = {
+  cancel,
+  check,
+  logo,
+  settings,
+  search,
+  upload,
+  share,
+  trash,
+  schedule,
+  expand,
+  drag,
+  menu,
+  profile,
+  back,
+  thumbnail,
+  tick,
+  lock,
+  plus,
+  scrollArrow,
+  succesTick,
+  file,
+  logoText,
+  create,
+  finance,
+  home,
+  xdcLogo,
+  change,
+  arrowDown,
+  requests,
+};
+
+export default icons;
---
+++ client/app/_assets/images/images.js
@@ -0,0 +1,7 @@
+import thumbnail from "./thumbnail.jpg";
+
+const images = {
+  thumbnail,
+};
+
+export default images;
