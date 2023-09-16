2f0319cdbac56d4308f62d39cd87aed19ae6a7b9
Primal <81525899+Seun2255@users.noreply.github.com>
2023-09-16 10:40:02 +0100

    feat: setup firebase app

---
+++ client/app/database.js
@@ -0,0 +1,116 @@
+import app from "@/firebase/firebaseApp";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  setDoc,
+  doc,
+} from "firebase/firestore";
+import { timeStamp } from "./utils/dateFunctions";
+// import { getTokenBalance } from "./api";
+
+const db = getFirestore(app);
+
+/**
+ * Auth Functions
+ */
+
+//Checks if the User exists in the database
+const checkIfUserExists = async (user) => {
+  var data = {};
+  const userData = await getDocs(collection(db, "users"));
+  userData.forEach((doc) => {
+    data[doc.id] = doc.data();
+  });
+  var state = false;
+  const addresses = Object.keys(data);
+  addresses.map((address) => {
+    if (address === user) state = true;
+  });
+  return state;
+};
+
+//creates a new User
+const createUser = async (address) => {
+  const user = {
+    username: "user",
+    about: "",
+    profilePic: "",
+    banner: "",
+    followers: [],
+    files: [],
+    requests: [],
+    address: address,
+    transactions: [],
+  };
+  var data = {};
+  const userData = await getDocs(collection(db, "users"));
+  userData.forEach((doc) => {
+    // doc.data() is never undefined for query doc snapshots
+    data[doc.id] = doc.data();
+  });
+  if (data[address]) {
+    console.log("User already registered");
+  } else {
+    await setDoc(doc(db, "users", address), user);
+  }
+};
+
+//updates a users data
+const updateUserProfile = async (
+  username,
+  about,
+  profilePic,
+  banner,
+  address
+) => {
+  var data = {};
+  const userData = await getDocs(collection(db, "users"));
+  userData.forEach((doc) => {
+    // doc.data() is never undefined for query doc snapshots
+    data[doc.id] = doc.data();
+  });
+  console.log(address);
+  var temp = data[address];
+  console.log(temp);
+  temp.about = about;
+  temp.username = username;
+  temp.profilePic = profilePic;
+  await setDoc(doc(db, "users", address), temp);
+};
+
+//gets a users data
+const getUserDetails = async (address) => {
+  var data = {};
+  const userData = await getDocs(collection(db, "users"));
+  userData.forEach((doc) => {
+    // doc.data() is never undefined for query doc snapshots
+    data[doc.id] = doc.data();
+  });
+  const details = data[address];
+  // const balances = await getTokenBalance(address);
+  // details.balance = balances.ethBalanceEther;
+  // details.tokenBalance = balances.tokenBalanceEther;
+  return details;
+};
+
+const recordTransaction = async (address, details) => {
+  var data = {};
+  const userData = await getDocs(collection(db, "users"));
+  userData.forEach((doc) => {
+    // doc.data() is never undefined for query doc snapshots
+    data[doc.id] = doc.data();
+  });
+  const user = data[address];
+  user.transactions.unshift(details);
+  await setDoc(doc(db, "users", address), user);
+};
+
+export {
+  updateUserProfile,
+  checkIfUserExists,
+  getUserDetails,
+  db,
+  recordTransaction,
+  createUser,
+};
---
+++ client/firebase/firebaseApp.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCkyA_sZX1w-JM4upSvRM7ehPsb62bPV1Y",
+  authDomain: "libra-396da.firebaseapp.com",
+  projectId: "libra-396da",
+  storageBucket: "libra-396da.appspot.com",
+  messagingSenderId: "518711458441",
+  appId: "1:518711458441:web:edbd9c735b3064cf966458",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
